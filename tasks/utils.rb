a44e2a1d6f6267e2816a64daa3abfd2fa0371c4d
Kevin Poorman <codefriar@users.noreply.github.com>
2021-01-09 22:42:40 -0500

    fix: new config commmand (#280)
    closes #278
    
    Co-authored-by: Denis Evers <denis-ev@users.noreply.github.com>

--- tasks/utils.rb
+++ tasks/utils.rb
@@ -34,7 +34,7 @@ module Utils
     command << convert_debug_enum(debug) unless convert_debug_enum(debug).size.zero?
     command << "-e \@#{@temp_config}" if playbook != 'playbook.config.yml'
     command << '--skip-tags setup' if already_setup?
-    command << '--skip-tags tor' unless decrypted_config_file[:enable_tor]
+    command << '--skip-tags tor' unless decrypted_config_file[:enable_tor] rescue false
     command << "-e config_dir=#{options[:config_dir]}"
     command << extra.to_s unless extra.nil? || extra.size.zero?
     command << '-i inventory'
